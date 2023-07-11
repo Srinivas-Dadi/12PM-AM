0029865cedf387e52dd2eea1592bf4cd8f8c08cf
SRINIVAS DADI <srivardhandadi@gmail.com>
2023-07-11 22:16:36 +0530

    home card data passing commit

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import Nav from '../Components/Nav/Nav'
+import Card from '../Components/Card/Card.jsx'
 import RestName from '../asserts/rest_name.png'
 import Burger from '../asserts/Burger.png'
 import Drink from '../asserts/Drinks.jpg'
 
 const Home = () => {
+  
+  const Menu =['Starter','Mojitos','Snacks','Main Course','Desserts']
+
+  // useEffect(() => {
+  //   // Menu = []
+  //   // setMenu(Menu)
+  //   console.log(Menu)
+  // },[])
+  
+  console.log(Menu)
+  
+
   return (
     <div className='homeScreen'>
         <Nav />
         <div className='Restraunt'>
-            <center><img src={RestName} alt="" srcset="" className='Restname' /></center> <br/>
+            <center><img src={RestName} alt="Restraunt Name" className='Restname' /></center> <br/>
             <center>
-              <img src={Drink} alt="" srcset="" className='Burger' />
-              <img src={Burger} alt="" srcset="" className='Burger' />
+              <img src={Drink} alt="drinks-vector"  className='Burger' />
+              <img src={Burger} alt="burger-vector" className='Burger' />
             </center>
         </div>
         <div className='category'>
@@ -22,7 +35,11 @@ const Home = () => {
             <h2>Main Course</h2>
             <h2>Desserts</h2>
         </div>
-        
+        <div className="mobilecard">
+          {Menu.map((id)=> {
+            return <Card item = {id}/>
+          })}
+        </div>
     </div>
   )
 }
