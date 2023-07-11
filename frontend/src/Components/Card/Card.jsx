d9d8efc10c975f7fec25e7e3938f4687182fae4f
SRINIVAS DADI <srivardhandadi@gmail.com>
2023-07-11 22:13:31 +0530

    home cards and data sending

---
+++ frontend/src/Components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './card.css'
+
+const Card = (props) => {
+    console.log(props.item)
+  return (
+    <div className='category-card'>
+        {/* <h1>{props.item}</h1><br/> */}
+
+    </div>
+  )
+}
+
+export default Card
